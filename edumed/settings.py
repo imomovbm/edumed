e911781b5217596d615851b90af97939324e20be
Bekzod Imomov <magiccodeuz@gmail.com>
2026-02-03 16:28:29 +0500

    check

--- edumed/settings.py
+++ edumed/settings.py
@@ -136,3 +136,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"  # For collectstatic
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#dd
