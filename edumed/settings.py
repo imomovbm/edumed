27b99a2eac8d345b7e4fe44aae087d00acc2e2c3
Bekzod Imomov <magiccodeuz@gmail.com>
2026-02-03 15:21:23 +0500

    added Allowed hosts

--- edumed/settings.py
+++ edumed/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['edumed.uz','www.edumed.uz','127.0.0.1']
 
 
 # Application definition
