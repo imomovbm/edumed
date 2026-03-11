890ff4cc9ba929a6b96a2f8ea076f654a4e44cfd
Bekzod Imomov <magiccodeuz@gmail.com>
2026-03-11 11:10:26 +0500

    SET SQL mode STRICT MODE

--- edumed/settings.py
+++ edumed/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
         "PORT": config("DB_PORT", default="3306"),
         "OPTIONS": {
             "charset": "utf8mb4",
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
